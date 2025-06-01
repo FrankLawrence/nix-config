441879feb6edcfb3730da81b5fc37068ff55dd21
FrankLawrence <frankl.am.htg@icloud.com>
2025-06-01 20:50:37 +0200

    Refacroting and mail module
    Moved files fit standard configuraiton structure and added mail module
    for hetzner VPS

---
+++ hosts/hetzner/configuration.nix
@@ -0,0 +1,79 @@
+{ pkgs, ... }:
+
+{
+  nix.settings = {
+    experimental-features = "nix-command flakes";
+  };
+
+  environment.systemPackages = with pkgs; [
+    neovim
+      git
+      age
+      agenix
+  ];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/boot";
+    fsType = "ext4";
+  };
+  swapDevices = [
+  {
+    device = "/dev/disk/by-label/swap";
+  }
+  ];
+
+  systemd.network.enable = true;
+  systemd.network.networks."30-wan" = {
+    matchConfig.Name = "enp1s0";
+    networkConfig.DHCP = "ipv4";
+    address = [
+      "2a01:4f9:c012:d8e::1/64"
+    ];
+    routes = [
+    { Gateway = "fe80::1"; }
+    ];
+  };
+
+  time.timeZone = "Europe/Helsinki";
+  i18n.defaultLocale = "en_US.UTF-8";
+  console.keyMap = "us";
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" "ext4" ];
+
+  users.users = {
+    root.hashedPassword = "!"; # Disable root login
+      pinkfloyd = {
+        isNormalUser = true;
+        extraGroups = [ "wheel" ];
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDLxYWRdvzd01GebtUTxUgLO6F5z/ricceiy6Gs6IU/H pinkfloyd@terra.local"
+        ];
+      };
+  };
+
+  programs.ssh.startAgent = true;
+
+  security.sudo = {
+    enable = true;
+    wheelNeedsPassword = false;
+  };
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 22 ];
+
+  system.stateVersion = "24.11";
+}
