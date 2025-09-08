040dc9271eed8a583ee3e15b114e858190049374
FrankLawrence <frankl.am.htg@icloud.com>
2025-09-09 00:25:01 +0200

    rekey with jupiter

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,20 +1,20 @@
 let
   local-terra = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDLxYWRdvzd01GebtUTxUgLO6F5z/ricceiy6Gs6IU/H";
-  users = [ local-terra ];
+  jupiter = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKc+QGEfGE7T+NcAN8EkDDx3bg5z2ucrecMwbkBve2RY frank@jupiter";
+
+  users = [ local-terra jupiter ];
 
   homelab = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMFhP+/afrYUNByay3MeBzmBc9nZcH8bEnIQ7Z0MsURO";
   systems = [ homelab ];
-
-  jupiter = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKc+QGEfGE7T+NcAN8EkDDx3bg5z2ucrecMwbkBve2RY frank@jupiter";
 in
 {
-  "sparkyfitness.age".publicKeys = [ local-terra homelab ];
-  "immich.age".publicKeys = [ local-terra homelab ];
-  "karakeep.age".publicKeys = [ local-terra homelab ];
-  "kavita.age".publicKeys = [ local-terra homelab ];
-  "komga.age".publicKeys = [ local-terra homelab ];
-  "mealie.age".publicKeys = [ local-terra homelab ];
-  "adguard.age".publicKeys = [ local-terra homelab ];
-  "pocket-id.age".publicKeys = [ local-terra homelab ];
-  "syncthing.age".publicKeys = [ local-terra homelab ];
+  "sparkyfitness.age".publicKeys = users ++ systems;
+  "immich.age".publicKeys        = users ++ systems;
+  "karakeep.age".publicKeys      = users ++ systems;
+  "kavita.age".publicKeys        = users ++ systems;
+  "komga.age".publicKeys         = users ++ systems;
+  "mealie.age".publicKeys        = users ++ systems;
+  "adguard.age".publicKeys       = users ++ systems;
+  "pocket-id.age".publicKeys     = users ++ systems;
+  "syncthing.age".publicKeys     = users ++ systems;
 }
